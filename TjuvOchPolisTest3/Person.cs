5ca6e1f520507b57dd443745d1f7c8c55cf26880
andreastollmar <115992410+andreastollmar@users.noreply.github.com>
2022-10-17 14:52:31 +0200

    Added Class person and subclasses

---
+++ TjuvOchPolisTest3/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TjuvOchPolisTest3
+{
+    internal class Person
+    {
+        //Add properties that apply to all subclasses
+
+    }
+
+
+    internal class Robber : Person
+    { 
+        //Add more code
+    }
+
+    internal class Citizen : Person
+    {
+        //add more code
+    }
+
+
+    internal class Police : Person
+    {
+        //Add more code and Constructors
+    }
+}
