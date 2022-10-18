a9fa23edfae9b4dced0a86e54b5183d1efe10ac2
andreastollmar <115992410+andreastollmar@users.noreply.github.com>
2022-10-18 13:04:52 +0200

    Uppdated stuff

--- TjuvOchPolisTest3/Stuff.cs
+++ TjuvOchPolisTest3/Stuff.cs
@@ -9,5 +9,25 @@ namespace TjuvOchPolisTest3
     internal class Stuff
     {
         public string[] stuff = { "Money", "Keys", "Cellphone", "Watch" };
+        static int robbed = 0;
+        static int arrested = 0;
+
+
+
+        public void StealItem(List<string> belongnings, List<string>stolen)
+        {
+            Random rnd = new Random();
+            int steal = rnd.Next(0, (belongnings.Count));
+            string taken = belongnings[steal];
+            stolen.Add(taken);
+            belongnings.RemoveAt(steal);
+            robbed++;            
+        }
+        public void TakeAllItems(List<string> stolen, List<string> retaken)
+        {
+            retaken.AddRange(stolen);
+            stolen.Clear();
+        }
+
     }
 }
