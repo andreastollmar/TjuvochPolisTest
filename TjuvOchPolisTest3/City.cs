2eb77b640a55b8525b2ed245684b64ff4939321e
MohammadAlzuabi <115696615+MohammadAlzuabi@users.noreply.github.com>
2022-10-18 11:10:25 +0200

    Added for loppar

--- TjuvOchPolisTest3/City.cs
+++ TjuvOchPolisTest3/City.cs
@@ -8,5 +8,36 @@ namespace TjuvOchPolisTest3
 {
     internal class City
     {
+        public string[,] city = new string[100, 25];
+        public static List<Robber> robbers = new List<Robber>();
+        public static List<Citizen> citizens = new List<Citizen>();
+        public static List<Police> polices = new List<Police>();
+
+
+        public static void Start()
+        {
+
+        }
+
+        public static void List()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Police police = new Police();
+                polices.Add(police);
+            }
+            for(int i = 0; i < 30; i++)
+            {
+                Citizen citizen = new Citizen();
+                citizens.Add(citizen);
+            }
+            for (int i = 0; i < 20; i++)
+            {
+                Robber robber = new Robber();
+                robbers.Add(robber);
+            }
+
+
+        }
     }
 }
